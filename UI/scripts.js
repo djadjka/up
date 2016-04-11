5868d8c0b0e8255ebcf5a48924a45fa5f9eee504
Aliaksei <djadjka.by@gmail.com>
2016-04-11 23:59:08 +0300

    add ajax and little fix

--- UI/scripts.js
+++ UI/scripts.js
@@ -1,32 +1,62 @@
 function Message(userNick, mesText, id, photoURL) {
-    this.nick = userNick;
-    this.mesText = mesText;
-    this.changed = false;
+    this.author = userNick;
+    this.text = mesText;
+    this.update = false;
     this.del = false;
-    this.photoURl = photoURL;
+    this.photoURL = photoURL;
     this.id = id;
 }
-
+var mainUrl = 'http://192.168.0.107:8080/chat';
 var messages = [];
 var delImgSrc = 'http://s1.iconbird.com/ico/1212/264/w16h161355246842delete6.png';
 var changeImgSrc = 'http://music.privet.ru/img/pics/edit-message-16x16.gif';
 var curentNick = '';
 var photoURL = '';
+var htmlImgDel = '\<div><button disabled>' +
+    '\<img disable src="' + delImgSrc + '"></div>';
+var htmlImgChe = '\<div><button disabled>' +
+    '\<img disable src="' + changeImgSrc + '"></div>';
+var isConnected = void 0;
 function run() {
     var appContainer = document.getElementsByClassName('container')[0];
     appContainer.addEventListener('click', delegateClickEvent);
     appContainer.addEventListener('keydown', delegateKeydownEvent);
-    restore();
+    Connect();
 }
 
-function restore() {
-    if (localStorage.getItem('messages')) {
+
+function Connect() {
+    restoreUser();
+    if (isConnected) {
+        return;
+    }
+    ajax('GET', mainUrl + '?token=TN11EN', null, function (responseText) {
+        messages = JSON.parse(responseText).messages;
+        printArrMess();
+    });
+    function whileConnected() {
+        isConnected = setTimeout(function () {
+            ajax('GET', mainUrl + '?token=TN11EN', null, function (responseText) {
+                if (isConnected) {
+                    var newMes = JSON.parse(responseText).messages;
+                    updatePage(newMes);
+                    whileConnected();
+                }
+            });
+        }, seconds(1));
+    }
+
+    whileConnected();
+}
+function seconds(value) {
+    return Math.round(value * 1000);
+}
+function restoreUser() {
+    if (localStorage.getItem('curentNick') || localStorage.getItem('photoURL')) {
         curentNick = localStorage.getItem('curentNick');
         photoURL = localStorage.getItem('photoURL');
         document.getElementsByClassName('userName')[0].value = curentNick;
         document.getElementsByClassName('selectPhoto')[0].value = photoURL;
-        messages = JSON.parse(localStorage.getItem('messages'));
-        printArrMess();
     }
 }
 function generateUUID() {
@@ -48,7 +78,7 @@ function delegateClickEvent(evtObj) {
         delMessage(evtObj);
     }
     if (evtObj.type === 'click' && classList1.contains('changeButton')) {
-        changeMessage(evtObj);
+        updateMessage(evtObj);
     }
     if (evtObj.type === 'click' && classList1.contains('logInButton')) {
         changeNick(evtObj);
@@ -71,6 +101,7 @@ function delegateKeydownEvent(evtObj) {
 
 function changeNick() {
     curentNick = document.getElementsByClassName('userName')[0].value.trim();
+    alert(curentNick);
     redrawing();
     if (document.getElementById('LoginCheckBox').checked) {
         localStorage.setItem('curentNick', curentNick);
@@ -86,34 +117,43 @@ function changeNick() {
 }
 
 function delMessage(evtObj) {
-    var img = '\<div><button disabled>' +
-        '\<img disable src="http://s1.iconbird.com/ico/1212/264/w16h161355246842delete6.png"></div>';
     var delMsgID = evtObj.target.parentNode.parentNode.parentNode.parentNode.id;
-    messages.forEach(function (item) {
-        if (item.id == delMsgID) {
-            item.del = true;
-            item.mesText = '';
-        }
-    });
-    setLocalStorage();
-    evtObj.target.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].innerHTML = '';
-    evtObj.target.parentNode.parentNode.parentNode.parentNode.childNodes[2].innerHTML = img;
 
-}
-
-function changeMessage(evtObj) {
-    var todoText = document.getElementsByClassName('entryField')[0];
-    if (todoText.value.trim()) {
-        var chMsgID = evtObj.target.parentNode.parentNode.parentNode.parentNode.id;
+    ajax('DELETE', mainUrl, '{"id":"' + delMsgID + '"}', function () {
         messages.forEach(function (item) {
-            if (item.id == chMsgID) {
-                item.changed = true;
-                item.mesText = todoText.value;
+            if (item.id == delMsgID) {
+                item.del = true;
+                item.text = '';
+                return;
             }
         });
-        evtObj.target.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].innerHTML = todoText.value;
-        todoText.value = '';
-        setLocalStorage();
+        evtObj.target.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].innerHTML = '';
+        evtObj.target.parentNode.parentNode.parentNode.parentNode.childNodes[2].innerHTML = htmlImgDel;
+    })
+}
+
+function updateMessage(evtObj) {
+    var todoText = document.getElementsByClassName('entryField')[0];
+    var currentMessage;
+    var chMsgID = evtObj.target.parentNode.parentNode.parentNode.parentNode.id;
+    if (todoText.value.trim()) {
+        messages.forEach(function (item) {
+            if (item.id === chMsgID) {
+                currentMessage = item;
+                return;
+            }
+        });
+        if (currentMessage.text == todoText.value) {
+            todoText.value = '';
+            return;
+        }
+        ajax('PUT', mainUrl, '{"id":"' + chMsgID + '", "text":"' + todoText.value + '"}', function () {
+            currentMessage.update = true;
+            currentMessage.text=todoText;
+            evtObj.target.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].innerHTML = todoText.value;
+            todoText.value = '';
+        });
+
     }
 }
 
@@ -122,6 +162,7 @@ function printArrMess() {
     for (var i = 0; i < messages.length; i++) {
         items.appendChild(createDivMessage(messages[i]));
     }
+    items.scrollTop += items.scrollHeight;
 }
 
 function addMyMessage() {
@@ -129,29 +170,25 @@ function addMyMessage() {
         if (!document.getElementsByClassName('entryField')[0].value.trim()) {
             return;
         }
-        createItem();
-        var divMes = createDivMessage(messages[messages.length - 1]);
-        var items = document.getElementsByClassName('messages')[0];
-        items.appendChild(divMes);
-        items.scrollTop += items.scrollHeight;
-
+        var mesText = document.getElementsByClassName('entryField')[0].value;
+        var message = new Message(curentNick, mesText, generateUUID(), photoURL);
+        ajax('POST', mainUrl, JSON.stringify(message), function () {
+            document.getElementsByClassName('entryField')[0].value = '';
+            messages.push(message);
+            var divMes = createDivMessage(messages[messages.length - 1]);
+            var items = document.getElementsByClassName('messages')[0];
+            items.appendChild(divMes);
+            items.scrollTop += items.scrollHeight;
+        });
     }
     else {
         alert('Enter user name');
     }
 }
 
-function createItem() {
-
-    var mesText = document.getElementsByClassName('entryField')[0].value;
-    document.getElementsByClassName('entryField')[0].value = '';
-    messages.push(new Message(curentNick, mesText, generateUUID(), photoURL));
-    setLocalStorage();
-}
-
 function createDivMessage(message) {
     var divItem = document.createElement('div');
-    if (message.nick == curentNick) {
+    if (message.author == curentNick) {
         divItem.classList.add('myMessage');
     }
     else {
@@ -168,7 +205,7 @@ function createLeft(message) {
     var left = document.createElement('div');
     var img = document.createElement('img');
     img.classList.add('photo');
-    img.setAttribute('src', message.photoURl);
+    img.setAttribute('src', message.photoURL);
     left.classList.add('leftColumn');
     left.appendChild(img);
     return left;
@@ -180,8 +217,8 @@ function createCenter(message) {
     name.classList.add('name');
     var text = document.createElement('div');
     center.classList.add('centerColumn');
-    name.appendChild(document.createTextNode(message.nick));
-    text.appendChild(document.createTextNode(message.mesText));
+    name.appendChild(document.createTextNode(message.author));
+    text.appendChild(document.createTextNode(message.text));
     center.appendChild(name);
     center.appendChild(text);
     return center;
@@ -197,13 +234,13 @@ function createRight(message) {
     var divChangeButton = document.createElement('div');
     var changeButton = document.createElement('button');
     var imgChangeButton = document.createElement('img');
-    if (message.nick == curentNick) {
+    if (message.author == curentNick) {
         imgDelButton.classList.add('delButton');
         imgDelButton.setAttribute('src', delImgSrc);
         delButton.appendChild(imgDelButton);
         divDelButton.appendChild(delButton);
         if (message.del) {
-            delButton.setAttribute('disable', 'disable');
+            delButton.setAttribute.disabled = true;
             right.appendChild(divDelButton);
         }
         else {
@@ -224,7 +261,7 @@ function createRight(message) {
             divDelButton.appendChild(delButton);
             right.appendChild(divDelButton);
         }
-        else if (message.changed) {
+        else if (message.update) {
             imgChangeButton.classList.add('changeButton');
             changeButton.disabled = true;
             imgChangeButton.setAttribute('src', changeImgSrc);
@@ -236,13 +273,11 @@ function createRight(message) {
     return right;
 }
 
-function setLocalStorage() {
-    localStorage.setItem('messages', JSON.stringify(messages));
-}
+
 function redrawing() {
     messages.forEach(function (item) {
         var div = document.getElementById(item.id);
-        if (item.nick == curentNick) {
+        if (item.author == curentNick) {
             div.classList.remove('message');
             div.classList.add('myMessage');
             div.replaceChild(createRight(item), div.lastChild);
@@ -253,6 +288,51 @@ function redrawing() {
             div.classList.add('message');
             div.replaceChild(createRight(item), div.lastChild);
         }
-
     });
+}
+function updatePage(newMes) {
+    for (var i = 0; i < messages.length; i++) {
+        var div = document.getElementById(messages[i].id);
+        if (messages[i].del !== newMes[i].del) {
+            div.childNodes[1].childNodes[1].innerHTML = '';
+            div.childNodes[2].innerHTML = htmlImgDel;
+            messages[i].del = true;
+            messages[i].text = '';
+        }
+        else if (newMes[i].update && messages[i].text !== newMes[i].text) {
+            messages[i].update = true;
+            messages[i].text = newMes[i].text;
+            div.childNodes[1].childNodes[1].innerHTML = newMes[i].text;
+            if (curentNick !== newMes[i].author) {
+                div.childNodes[2].innerHTML = htmlImgChe;
+            }
+        }
+    }
+    if (messages.length < newMes.length) {
+        for (var i = messages.length; i < newMes.length; i++) {
+            var divMes = createDivMessage(newMes[i]);
+            messages.push(newMes[i]);
+            var items = document.getElementsByClassName('messages')[0];
+            items.appendChild(divMes);
+            items.scrollTop += items.scrollHeight;
+        }
+    }
+}
+
+function ajax(method, url, data, continueWith) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method || 'GET', url, true);
+    xhr.onload = function () {
+        if (xhr.readyState !== 4)
+            return;
+
+        if (xhr.status != 200) {
+            return;
+        }
+        continueWith(xhr.responseText);
+    };
+    xhr.onerror = function () {
+        document.getElementsByClassName('error')[0].style.display = 'block';
+    };
+    xhr.send(data);
 }
